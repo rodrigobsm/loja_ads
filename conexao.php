ed83523427580091c3d5a2ca581f64cecaaaf19f
Aluno Unifeob <Aluno Unifeob@192.168.7.211>
2016-05-02 21:48:56 -0300

    Corrigido merge.

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(0);
-ini_set('display_errors', 0);
+//error_reporting(0);
+//ini_set('display_errors', 0);
 
 // conexao ao banco
 $conexao = mysql_connect("192.168.7.129", "root", "vertrigo");
