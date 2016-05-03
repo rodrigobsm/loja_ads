d91835a4bfab4c8442cfccece0fcbbcdee7b0316
Giuliano Cesar Casline <giulianocasline@gmail.com>
2016-05-02 22:23:11 -0300

    Tela de Login - Alteração 2 - Formulario - tag <...class=form-signin">

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 
 <div class="container">
 
-      <form action="validar.php" method="post">
+      <form action="validar.php" method="Post" class="form-signin">
         <h2 class="form-signin-heading" align="center">Bem-Vindo</h2>
         <label for="inputEmail" class="sr-only">email</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Digite seu email" required="" autofocus="">
