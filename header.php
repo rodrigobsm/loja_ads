b4fcbac7249114ecb60daecc852c399f36e831ef
Giuliano Cesar Casline <giulianocasline@gmail.com>
2016-05-02 21:56:38 -0300

    Tela de Login - Front

--- header.php
+++ header.php
@@ -19,6 +19,8 @@
 
     <!-- Custom styles for this template -->
     <link href="assets/css/justified-nav.css" rel="stylesheet">
+    
+    <link href="assets/css/signin.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
