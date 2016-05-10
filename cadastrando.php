d946a1e2b486d3e5033a1fb805a0eacbb406577e
“EduardoAugusto2015” <eduardopetreca@gmail.com>
2016-05-09 21:45:51 -0300

    senha MD5

--- cadastrando.php
+++ cadastrando.php
@@ -31,7 +31,8 @@
 			echo "<script> alert('Email já cadastrado! Por favor utilize outro email ou tente recuperar sua senha!'); window.location.replace('cadastro.php'); </script>";
 		}else{
 			// cadastra usuario no banco e redireciona para tela de login
-			$queryCadastro = "INSERT INTO clientes (nome, email, senha, cpf, tel_area, tel_numero, cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('$nome', '$email','md5($senha)','$cpf','$tel_area','$tel_numero','$cep','$logradouro','$numero','$complemento','$bairro','$cidade','$uf')";
+			$senha = md5($senha);
+			$queryCadastro = "INSERT INTO clientes (nome, email, senha, cpf, tel_area, tel_numero, cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('$nome', '$email','$senha','$cpf','$tel_area','$tel_numero','$cep','$logradouro','$numero','$complemento','$bairro','$cidade','$uf')";
 			mysql_query($queryCadastro,$conexao);
 			echo "<script> alert('Cadastro realizado com sucesso por favor realize login no sistema!'); window.location.replace('login.php'); </script>";
 		}
