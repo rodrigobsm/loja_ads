05f380d0817c49b4cb5c1bbf0e87fddcc3b074db
“EduardoAugusto2015” <eduardopetreca@gmail.com>
2016-05-09 21:26:46 -0300

    nova versão

--- cadastrando.php
+++ cadastrando.php
@@ -5,7 +5,7 @@
 	// recebe os dados do formulário 
 	$nome=mysql_real_escape_string($_POST['nome']);
 	$email=mysql_real_escape_string($_POST['email']);
-	$senha=md5($_POST['senha']);
+	$senha=mysql_real_escape_string($_POST['senha']);
 	$cpf=mysql_real_escape_string($_POST['cpf']);
 	$tel_area=mysql_real_escape_string($_POST['tel_area']);
 	$tel_numero=mysql_real_escape_string($_POST['tel_numero']);
@@ -31,7 +31,7 @@
 			echo "<script> alert('Email já cadastrado! Por favor utilize outro email ou tente recuperar sua senha!'); window.location.replace('cadastro.php'); </script>";
 		}else{
 			// cadastra usuario no banco e redireciona para tela de login
-			$queryCadastro = "INSERT INTO clientes (nome, email, senha, cpf, tel_area, tel_numero, cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('$nome', '$email','$senha','$cpf','$tel_area','$tel_numero','$cep','$logradouro','$numero','$complemento','$bairro','$cidade','$uf')";
+			$queryCadastro = "INSERT INTO clientes (nome, email, senha, cpf, tel_area, tel_numero, cep, logradouro, numero, complemento, bairro, cidade, uf) VALUES ('$nome', '$email','md5($senha)','$cpf','$tel_area','$tel_numero','$cep','$logradouro','$numero','$complemento','$bairro','$cidade','$uf')";
 			mysql_query($queryCadastro,$conexao);
 			echo "<script> alert('Cadastro realizado com sucesso por favor realize login no sistema!'); window.location.replace('login.php'); </script>";
 		}
